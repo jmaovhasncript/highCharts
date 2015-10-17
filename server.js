40eb4cbce6d2b42b896908f9f9dd035675754926
Mohan Kumar <mohankumar@Mohans-MacBook-Pro.local>
2015-10-17 13:11:31 +0530

    rewriting logic

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ csv.fromStream(cluster_stream, {headers : true,ignoreEmpty: true}).on("data", fu
     //console.log(data);
 }).on("end", function(){
     console.log("done");
-    console.log(cluster_location);
+    //console.log(cluster_location);
 });
 
 
@@ -37,18 +37,47 @@ csv.fromStream(cluster_disk, {headers : true,ignoreEmpty: true}).on("data", func
     //console.log(data);
 }).on("end", function(){
     console.log("done");
-    console.log(cluster_disk_util);
+    //console.log(cluster_disk_util);
 
 });
+
+var location_precpctive ={
+
+}
 
 app.get('/totalUsage', function(req, res){
     debugger;
 
     for(var key in cluster_location){
         cluster_location[key].diskUsage = parseInt(cluster_disk_util[key].data["disk_usage(MB)"]);
-        cluster_location[key].timeStanp = cluster_disk_util[key].data["timestamp"];
+        cluster_location[key].timeStamp = cluster_disk_util[key].data["timestamp"];
     }
-    res.status(200).send({totalUsage : totalDiskUtils,diskUtils :cluster_location});
+
+    //console.log(cluster_location);
+
+    for(key in cluster_location){
+        console.log(cluster_location[key]);
+
+        if(!location_precpctive[cluster_location[key].data.country_code]){
+            location_precpctive[cluster_location[key].data.country_code] = {
+                country_code : cluster_location[key].data.country_code ,
+                cluster_id :[cluster_location[key].data.cluster_id],
+                disk_usage:[cluster_location[key].diskUsage],
+                timeStamp :[cluster_location[key].timeStamp]
+            }
+        }else{
+            location_precpctive[cluster_location[key].data.country_code].cluster_id.push(cluster_location[key].data.cluster_id);
+            location_precpctive[cluster_location[key].data.country_code].disk_usage.push(cluster_location[key].diskUsage);
+            location_precpctive[cluster_location[key].data.country_code].timeStamp.push(cluster_location[key].timeStamp);
+
+        }
+    }
+
+    console.log(location_precpctive);
+
+
+
+    res.status(200).send({totalUsage : totalDiskUtils,diskUtils :location_precpctive});
     res.end();
 });
 
